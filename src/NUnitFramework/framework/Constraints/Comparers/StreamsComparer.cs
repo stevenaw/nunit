7f2bf2140068d3daa77f80c5c06204b3fdc1723e
stevenaw <sweerdenburg@gmail.com>
2022-03-15 20:37:17 -0400

    use pattern matching in the comparers

--- src/NUnitFramework/framework/Constraints/Comparers/StreamsComparer.cs
+++ src/NUnitFramework/framework/Constraints/Comparers/StreamsComparer.cs
@@ -21,11 +21,8 @@ namespace NUnit.Framework.Constraints.Comparers
 
         public bool? Equal(object x, object y, ref Tolerance tolerance, ComparisonState state)
         {
-            if (!(x is Stream) || !(y is Stream))
+            if (!(x is Stream xStream) || !(y is Stream yStream))
                 return null;
-
-            Stream xStream = (Stream)x;
-            Stream yStream = (Stream)y;
 
             if (xStream == yStream) return true;
 
